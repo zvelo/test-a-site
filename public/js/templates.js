ee4993b8463bda8944abaeaa351d215f30691722
Joshua Rubin <jrubin@zvelo.com>
2013-07-18 17:41:38 -0600

    update the styling of the results page
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- public/js/templates.js
+++ public/js/templates.js
@@ -123,15 +123,15 @@ function program4(depth0,data) {
 function program6(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n  <div class=\"span2 reputation\">\n    <div class=\"aspect\"></div>\n    <div class=\"img reputation";
+  buffer += "\n  <div class=\"span3\">\n    <div class=\"reputation\">\n      <div class=\"aspect\"></div>\n      <div class=\"img reputation";
   if (stack1 = helpers.reputationId) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.reputationId; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\"></div>\n    <p>";
+    + "\"></div>\n      <p>";
   if (stack1 = helpers.reputation) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.reputation; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</p>\n  </div>\n  ";
+    + "</p>\n    </div>\n  </div>\n  ";
   return buffer;
   }
 
@@ -141,7 +141,7 @@ function program8(depth0,data) {
   return "\n  <button class=\"btn btn-large btn-warning report\" title=\"Report this URL as miscategorized\">Miscategorized?</button>\n  ";
   }
 
-  buffer += "<div class=\"inner-container row-fluid\">\n  <div class=\"span10\">\n    <p class=\"lead url\" title=\"";
+  buffer += "<div class=\"inner-container row-fluid\">\n  <div class=\"span9\">\n    <p class=\"lead url\" title=\"";
   if (stack1 = helpers.url) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.url; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
